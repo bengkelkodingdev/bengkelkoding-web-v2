7fcb0703617902b5436b5ac02dd74539d9353f1c
aslam thariq <aslamthariq01@gmail.com>
2024-07-03 10:19:23 +0700

    commit lokal editform

--- app/component/general/EditForm.tsx
+++ app/component/general/EditForm.tsx
@@ -77,12 +77,7 @@ const EditForm: React.FC<EditFormProps> = ({ user, onSave }) => {
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-96 p-2.5 "/>
       </div>
     </div>
-      <button
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        onClick={handleSave}
-      >
-        Save
-      </button>
+     
     </div>
   );
 };
