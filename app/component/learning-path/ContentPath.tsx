f2ec50920211a6145caefd2cab0200872e14b0c4
Hypes-Astro <muhamadalifanwar@gmail.com>
2024-08-17 22:57:16 +0700

    redesign learning path

--- app/component/learning-path/ContentPath.tsx
+++ app/component/learning-path/ContentPath.tsx
@@ -70,13 +70,17 @@ const ContentPath = ({ selectedMenu }) => {
         <Button text="Daftar Kelas" className="w-[12vw] mt-4" />
       </div>
       {/* Path content */}
-      <section className=" w-full h-full flex justify-between gap-24 mt-14">
-        <div className="kursus-content flex flex-col gap-10  w-full">
-          {dataPath.data.roadmap.map((_, index) => (
-            <div className="card-kursus border p-7 flex flex-col gap-2 ">
+      <section className="w-full h-full flex flex-col gap-6 mt-14">
+        {dataPath.data.roadmap.map((_, index) => (
+          <div
+            key={index}
+            className="card-kursus flex flex-col md:flex-row  p-7 gap-8"
+          >
+            {/* Left Section */}
+            <div className="flex flex-col w-full md:w-1/2 border p-3 gap-4">
               <div className="flex justify-between">
-                {/* level */}
-                <div className="level flex">
+                {/* Level */}
+                <div className="level flex items-center">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     height="24px"
@@ -86,10 +90,13 @@ const ContentPath = ({ selectedMenu }) => {
                   >
                     <path d="M200-160v-240h120v240H200Zm240 0v-440h120v440H440Zm240 0v-640h120v640H680Z" />
                   </svg>
-                  <p> {dataPath.data.roadmap[index].course.level}</p>
+                  <p className="ml-2">
+                    {" "}
+                    {dataPath.data.roadmap[index].course.level}
+                  </p>
                 </div>
-                {/* rating */}
-                <div className="rating flex items-center ">
+                {/* Rating */}
+                <div className="rating flex items-center">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     height="20px"
@@ -99,66 +106,52 @@ const ContentPath = ({ selectedMenu }) => {
                   >
                     <path d="m305-704 112-145q12-16 28.5-23.5T480-880q18 0 34.5 7.5T543-849l112 145 170 57q26 8 41 29.5t15 47.5q0 12-3.5 24T866-523L756-367l4 164q1 35-23 59t-56 24q-2 0-22-3l-179-50-179 50q-5 2-11 2.5t-11 .5q-32 0-56-24t-23-59l4-165L95-523q-8-11-11.5-23T80-570q0-25 14.5-46.5T135-647l170-57Z" />
                   </svg>
-                  <p> {dataPath.data.roadmap[index].course.rating}</p>
+                  <p className="ml-2">
+                    {dataPath.data.roadmap[index].course.rating}
+                  </p>
                 </div>
               </div>
               <p className="title-kursus font-semibold text-lg">
                 {dataPath.data.roadmap[index].course.title}
               </p>
-              <p className="title-kursus font-normal text-lg mb-2">
+              <p className="font-normal text-lg mb-2">
                 {dataPath.data.roadmap[index].course.brief_description}
               </p>
-              <div className="flex flex-col">
-                <div className="infoDetail flex gap-5 justify-between w-full">
-                  <div className="infoDetail-down flex justify-between gap-5">
-                    {/* modul */}
-                    <div className="flex items-center">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        height="20px"
-                        viewBox="0 -960 960 960"
-                        width="24px"
-                        fill="#999999"
-                      >
-                        <path d="M707-500q-33 0-56.5-23.5T627-580v-100q0-33 23.5-56.5T707-760h53q33 0 56.5 23.5T840-680v100q0 33-23.5 56.5T760-500h-53Zm-253 0q-33 0-56.5-23.5T374-580v-100q0-33 23.5-56.5T454-760h53q33 0 56.5 23.5T587-680v100q0 33-23.5 56.5T507-500h-53Zm-253 0q-33 0-56.5-23.5T121-580v-100q0-33 23.5-56.5T201-760h53q33 0 56.5 23.5T334-680v100q0 33-23.5 56.5T254-500h-53Zm0 300q-33 0-56.5-23.5T121-280v-100q0-33 23.5-56.5T201-460h53q33 0 56.5 23.5T334-380v100q0 33-23.5 56.5T254-200h-53Zm253 0q-33 0-56.5-23.5T374-280v-100q0-33 23.5-56.5T454-460h53q33 0 56.5 23.5T587-380v100q0 33-23.5 56.5T507-200h-53Zm253 0q-33 0-56.5-23.5T627-280v-100q0-33 23.5-56.5T707-460h53q33 0 56.5 23.5T840-380v100q0 33-23.5 56.5T760-200h-53Z" />
-                      </svg>
-                      <p>
-                        {" "}
-                        {
-                          dataPath.data.roadmap[index].course.module_count
-                        } Modul{" "}
-                      </p>
-                    </div>
-
-                    {/* jumlah mhs */}
-                    <div className="flex items-center">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        height="20px"
-                        viewBox="0 -960 960 960"
-                        width="24px"
-                        fill="#999999"
-                      >
-                        <path d="M840-280v-276L480-360 40-600l440-240 440 240v320h-80ZM480-120 200-272v-200l280 152 280-152v200L480-120Z" />
-                      </svg>
-                      <p>
-                        {" "}
-                        {dataPath.data.roadmap[index].course.student_count} Mhs
-                      </p>
-                    </div>
-                  </div>
+              <div className="flex justify-between">
+                {/* Modul */}
+                <div className="flex items-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    height="20px"
+                    viewBox="0 -960 960 960"
+                    width="24px"
+                    fill="#999999"
+                  >
+                    <path d="M707-500q-33 0-56.5-23.5T627-580v-100q0-33 23.5-56.5T707-760h53q33 0 56.5 23.5T840-680v100q0 33-23.5 56.5T760-500h-53Zm-253 0q-33 0-56.5-23.5T374-580v-100q0-33 23.5-56.5T454-760h53q33 0 56.5 23.5T587-680v100q0 33-23.5 56.5T507-500h-53Zm-253 0q-33 0-56.5-23.5T121-580v-100q0-33 23.5-56.5T201-760h53q33 0 56.5 23.5T334-680v100q0 33-23.5 56.5T254-500h-53Zm0 300q-33 0-56.5-23.5T121-280v-100q0-33 23.5-56.5T201-460h53q33 0 56.5 23.5T334-380v100q0 33-23.5 56.5T254-200h-53Zm253 0q-33 0-56.5-23.5T374-280v-100q0-33 23.5-56.5T454-460h53q33 0 56.5 23.5T587-380v100q0 33-23.5 56.5T507-200h-53Zm253 0q-33 0-56.5-23.5T627-280v-100q0-33 23.5-56.5T707-460h53q33 0 56.5 23.5T840-380v100q0 33-23.5 56.5T760-200h-53Z" />
+                  </svg>
+                  <p className="ml-2">
+                    {dataPath.data.roadmap[index].course.module_count} Modul
+                  </p>
+                </div>
+                {/* Jumlah Mhs */}
+                <div className="flex items-center">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    height="20px"
+                    viewBox="0 -960 960 960"
+                    width="24px"
+                    fill="#999999"
+                  >
+                    <path d="M840-280v-276L480-360 40-600l440-240 440 240v320h-80ZM480-120 200-272v-200l280 152 280-152v200L480-120Z" />
+                  </svg>
+                  <p className="ml-2">
+                    {dataPath.data.roadmap[index].course.student_count} Mhs
+                  </p>
                 </div>
               </div>
             </div>
-          ))}
-          {/*  */}
-        </div>
-        {/* batas */}
-        <div className="hidden md:block w-1 rounded-t-full bg-blue-800 mx-4"></div>{" "}
-        {/* step */}
-        <div className="step-content flex flex-col gap-10 w-full">
-          {dataPath.data.roadmap.map((_, index) => (
-            <div className="card-kursus  flex flex-col gap-1">
+            {/* Right Section */}
+            <div className="flex flex-col w-full md:w-1/2 border p-3 ">
               <p className="numberKursus font-semibold text-lg">
                 {dataPath.data.roadmap[index].title}
               </p>
@@ -166,12 +159,10 @@ const ContentPath = ({ selectedMenu }) => {
                 {dataPath.data.roadmap[index].description}
               </p>
             </div>
-          ))}
-          {/* dibikin card aja nanti di loop kasih props juga */}
-
-          {/* end card */}
-        </div>
+          </div>
+        ))}
       </section>
+
       {/* daftar kelas */}
       <section className="list-kelas my-12">
         <h2 className="text-center mb-4">Daftar kelas</h2>
