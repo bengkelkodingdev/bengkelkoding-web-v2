7593d2a0732c4dc3d8099844cdca3443245e4a9f
arifsaputradev <arifsaputradev@gmail.com>
2024-08-26 10:58:07 +0700

    add url to get public image

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["bengkod-api.rayhanashlikh.my.id", "mahasiswa.dinus.ac.id"],
+    domains: [
+      "bengkod-api.rayhanashlikh.my.id",
+      "mahasiswa.dinus.ac.id",
+      "simpeg.dinus.ac.id",
+    ],
   },
 };
 
