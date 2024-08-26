463c24b36728b11208daed3c514db0cd0e02bb08
arifsaputradev <arifsaputradev@gmail.com>
2024-08-26 10:50:17 +0700

    fix url to setting

--- app/component/dashboard/student/Header.tsx
+++ app/component/dashboard/student/Header.tsx
@@ -109,7 +109,7 @@ const StudentHeader = () => {
               >
                 <li>
                   <a
-                    href="setting"
+                    href="/dashboard/student/setting"
                     className="text-xs md:text-sm block px-4 py-2 hover:bg-gray-100"
                   >
                     Pengaturan
