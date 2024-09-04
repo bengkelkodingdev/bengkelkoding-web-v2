62f4e8eafca913299415cd76218fa3b881ef7e22
Hypes-Astro <muhamadalifanwar@gmail.com>
2024-09-04 21:50:30 +0700

    [ROLE] - Limitation of user feature in list classroom

--- app/dashboard/[user]/kelas/page.tsx
+++ app/dashboard/[user]/kelas/page.tsx
@@ -369,8 +369,8 @@ const HomeDashboardKelasPage = () => {
                               <path d="M12 4C7 4 2.73 7.11 1 11.5 2.73 15.89 7 19 12 19s9.27-3.11 11-7.5C21.27 7.11 17 4 12 4zm0 12.5c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5zm0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3z" />
                             </svg>
                           </Link>
-                          {/* tombol edit, gunakan aja query untuk href passing data id classroom ke edit */}
-                          {role_user == "superadmin" || role_user == "admin" ? (
+                          {/* Aksi */}
+                          {role_user == "superadmin" ? (
                             <>
                               <Link
                                 href={{
@@ -405,6 +405,29 @@ const HomeDashboardKelasPage = () => {
                                 >
                                   <path d="M0 0h24v24H0V0z" fill="none" />
                                   <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V9c0-1.1-.9-2-2-2H8c-1.1 0-2 .9-2 2v10zM18 4h-2.5l-.71-.71c-.18-.18-.44-.29-.7-.29H9.91c-.26 0-.52.11-.7.29L8.5 4H6c-.55 0-1 .45-1 1s.45 1 1 1h12c.55 0 1-.45 1-1s-.45-1-1-1z" />
+                                </svg>
+                              </Link>
+                            </>
+                          ) : role_user == "admin" ? (
+                            <>
+                              <Link
+                                href={{
+                                  pathname: `kelas/tambah`, // atau halaman yang sesuai jika berbeda
+                                  query: {
+                                    idClassroom: classroom.id,
+                                    status: "edit",
+                                  },
+                                }}
+                                className="block bg-yellow2 p-1 rounded-md fill-white hover:bg-yellow1 transition-all ease-in-out duration-150"
+                              >
+                                <svg
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  height="18px"
+                                  viewBox="0 0 24 24"
+                                  width="18px"
+                                >
+                                  <path d="M0 0h24v24H0V0z" fill="none" />
+                                  <path d="M3 17.46v3.04c0 .28.22.5.5.5h3.04c.13 0 .26-.05.35-.15L17.81 9.94l-3.75-3.75L3.15 17.1c-.1.1-.15.22-.15.36zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z" />
                                 </svg>
                               </Link>
                             </>
