f01f8ff0cab77013fb4b64dacb829bbcbf0ff502
arifsaputradev <arifsaputradev@gmail.com>
2024-09-08 16:48:42 +0700

    consume get data detail assignment, post upload data and confirm submit task

--- app/api/student/dashboard.ts
+++ app/api/student/dashboard.ts
@@ -1,5 +1,5 @@
 import { AxiosResponse } from "axios";
-import { createRequest } from "../request";
+import { createPostRequest, createRequest } from "../request";
 
 // Get Statistics
 export const getStudentStatistics = async (): Promise<AxiosResponse> =>
@@ -19,3 +19,37 @@ export const getStudentClassroomDetail = async (
 export const getStudentAssignments = async (): Promise<AxiosResponse> =>
   createRequest(`/api/v1/student/dashboard/assignments`);
 
+// Get Detail Assignment
+export const getStudentAssignmentDetail = async (
+  classroom_id: string,
+  assignment_id: string
+): Promise<AxiosResponse> =>
+  createRequest(
+    `/api/v1/student/classrooms/${classroom_id}/assignments/${assignment_id}/detail`
+  );
+
+// Post Upload Task
+export const postUploadTask = async (
+  classroom_id: string,
+  assignment_id: string,
+  answer_file: File,
+  comment: string
+): Promise<AxiosResponse> => {
+  const formData = new FormData();
+  formData.append("answer_file", answer_file);
+  formData.append("comment", comment);
+
+  return await createPostRequest(
+    `/api/v1/mobile/student/assignment/classroom/${classroom_id}/assignments/${assignment_id}/task`,
+    formData
+  );
+};
+
+// Post Submit Task
+export const postSubmitTask = async (
+  classroom_id: string,
+  assignment_id: string
+): Promise<AxiosResponse> =>
+  createPostRequest(
+    `/api/v1/mobile/student/assignment/classroom/${classroom_id}/assignments/${assignment_id}/task/submit`
+  );
