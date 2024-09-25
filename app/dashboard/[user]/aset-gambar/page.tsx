78ef0824e9849f0586c18478e70e2ed12668ba1a
Hypes-Astro <muhamadalifanwar@gmail.com>
2024-09-25 10:23:52 +0700

    [RECONS] - Reconstruction Search and Pagination in Absence Feature

--- app/dashboard/[user]/aset-gambar/page.tsx
+++ app/dashboard/[user]/aset-gambar/page.tsx
@@ -284,50 +284,6 @@ const HomeDashboardAsetGambarPage = () => {
             )}
           </tbody>
         </table>
-        {/* <nav
-          className="flex items-center flex-column flex-wrap md:flex-row justify-between pt-4"
-          aria-label="Table navigation"
-        >
-          <span className="text-sm font-normal text-neutral3 mb-4 md:mb-0 block w-full md:inline md:w-auto">
-            Menampilkan halaman {currentPage} dari {totalPages}
-          </span>
-          <ul className="inline-flex -space-x-px rtl:space-x-reverse text-sm h-8">
-            <li>
-              <button
-                onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))}
-                className="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-neutral3 bg-white border border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-neutral2"
-                disabled={currentPage === 1}
-              >
-                Sebelumnya
-              </button>
-            </li>
-            {[...Array(totalPages)].map((_, index) => (
-              <li key={index}>
-                <button
-                  onClick={() => setCurrentPage(index + 1)}
-                  className={`flex items-center justify-center px-3 h-8 leading-tight ${
-                    currentPage === index + 1
-                      ? "text-blue-600 bg-blue-50 border-blue-300"
-                      : "text-neutral3 bg-white border-gray-300 hover:bg-gray-100 hover:text-neutral2"
-                  }`}
-                >
-                  {index + 1}
-                </button>
-              </li>
-            ))}
-            <li>
-              <button
-                onClick={() =>
-                  setCurrentPage((prev) => Math.min(prev + 1, totalPages))
-                }
-                className="flex items-center justify-center px-3 h-8 leading-tight text-neutral3 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-neutral2"
-                disabled={currentPage === totalPages}
-              >
-                Selanjutnya
-              </button>
-            </li>
-          </ul>
-        </nav> */}
 
         <Pagination
           totalPages={totalPages}
