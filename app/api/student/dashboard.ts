88b76d8fbdfcb52089064570439163f66c8d06a1
arifsaputradev <arifsaputradev@gmail.com>
2024-09-29 20:51:48 +0700

    add put student profile

--- app/api/student/dashboard.ts
+++ app/api/student/dashboard.ts
@@ -1,5 +1,5 @@
 import { AxiosResponse } from "axios";
-import { createPostRequest, createRequest } from "../request";
+import { createPostRequest, createPutRequest, createRequest } from "../request";
 
 // Get Statistics
 export const getStudentStatistics = async (): Promise<AxiosResponse> =>
@@ -63,3 +63,15 @@ export const getPresences = async (
   classroom_id: number
 ): Promise<AxiosResponse> =>
   createRequest(`/api/v1/student/classrooms/${classroom_id}/presences`);
+
+// Put Profile
+export const putStudentProfile = async (
+  old_password: string,
+  new_password: string,
+  retype_password: string
+): Promise<AxiosResponse> =>
+  createPutRequest("/api/v1/auth/profile", {
+    old_password: old_password,
+    new_password: new_password,
+    retype_password: retype_password,
+  });
