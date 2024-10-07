823a1b762bed849de6e3683176de174a52b58c2a
arifsaputradev <arifsaputradev@gmail.com>
2024-10-07 13:57:13 +0700

    fix today data

--- app/dashboard/student/page.tsx
+++ app/dashboard/student/page.tsx
@@ -122,7 +122,27 @@ const StudentPage = () => {
     }
   };
 
-  const today = new Date().toISOString().slice(0, 19).replace("T", " ");
+  const todayDate = new Date()
+    .toLocaleString("en-US", {
+      timeZone: "Asia/Jakarta",
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+      hour12: false,
+    })
+    .replace(/\//g, "-")
+    .replace(",", "");
+  const today =
+    todayDate.slice(6, 10) +
+    "-" +
+    todayDate.slice(0, 2) +
+    "-" +
+    todayDate.slice(3, 5) +
+    " " +
+    todayDate.slice(11);
 
   if (isLoading) {
     return (
