edffaef06c4c3b6cfcfa9bbfc1af002074e32b60
arifsaputradev <arifsaputradev@gmail.com>
2024-10-31 23:50:03 +0700

    make layout color mode light

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" data-color-mode="light">
       <body className={poppins.className}>{children}</body>
     </html>
   );
