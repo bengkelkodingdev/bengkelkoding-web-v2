3672b99174f2f4e09e7babcd8d891a95f8fbfd4a
arifsaputradev <arifsaputradev@gmail.com>
2024-11-02 19:13:03 +0700

    consume crud admin user

--- app/dashboard/[user]/pengguna/admin/page.tsx
+++ app/dashboard/[user]/pengguna/admin/page.tsx
@@ -1,10 +1,87 @@
+"use client";
 import Link from "next/link";
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
+import Cookies from "js-cookie";
+
+import "react-toastify/dist/ReactToastify.css";
+import { toast, ToastContainer } from "react-toastify";
+import { AdminResponse } from "@/app/interface/UserManagement";
+import Pagination from "@/app/component/general/PaginationCustom";
+import { deleteUserAdmin, getUserAdmin } from "@/app/api/superadmin/userAdmin";
+import Modal from "@/app/component/general/Modal";
+import Input from "@/app/component/general/Input";
+import Button from "@/app/component/general/Button";
 
 const HomeDashboardPenggunaAdmin = () => {
+  const access_token = Cookies.get("access_token");
+  const role_user = Cookies.get("user_role");
+
+  const [DataAdmin, setDataAdmin] = useState<AdminResponse>();
+
+  const [searchTerm, setSearchTerm] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+  const itemsPerPage = 10; // Jumlah data per halaman
+
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(e.target.value);
+    setCurrentPage(1);
+  };
+
+  const fetchData = useCallback(async () => {
+    if (!access_token) {
+      throw new Error("Access token not found");
+    }
+    try {
+      let response;
+      if (role_user && (role_user === "superadmin" || role_user === "admin")) {
+        response = await getUserAdmin(searchTerm, currentPage, itemsPerPage);
+      }
+
+      if (response) {
+        setDataAdmin(response);
+        setTotalPages(response.meta.pagination.total_pages); // Set total halaman dari meta pagination API
+      }
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  }, [role_user, searchTerm, access_token, currentPage, itemsPerPage]);
+
+  // useEffect untuk memanggil fetchData setiap kali searchTerm berubah
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]); // Jalankan fetchData saat searchTerm berubah
+
+  const [adminId, setAdminId] = useState(0);
+  const [adminName, setAdminName] = useState("");
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const handleOpenModal = (adminId: number, adminName: string) => {
+    setAdminId(adminId);
+    setAdminName(adminName);
+    setIsModalOpen(true);
+  };
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleDelete = async () => {
+    try {
+      await deleteUserAdmin(adminId);
+      toast.success(`Berhasil Menghapus Admin ${adminName} 😁`);
+      fetchData();
+      handleCloseModal();
+    } catch (error) {
+      console.error("Failed to delete classroom", error);
+      toast.error(`Gagal Menghapus Admin 😔: ${error.message}`);
+      if (error instanceof Error) {
+        console.error("Error message:", error.message);
+      }
+    }
+  };
+
   return (
     <>
-      <div className="relative overflow-x-auto">
+      <div className="overflow-x-auto ">
         {/* Searching + Button */}
         <div className="flex flex-column sm:flex-row flex-wrap space-y-4 sm:space-y-0 items-center justify-between pb-4">
           {/* Search */}
@@ -28,6 +105,7 @@ const HomeDashboardPenggunaAdmin = () => {
             <input
               type="text"
               id="table-search"
+              onChange={handleSearchChange}
               className="block w-[200px] lg:w-[300px] p-2 ps-10 border border-neutral4 rounded-md text-neutral1 focus:outline-none focus:ring-4 focus:ring-primary5 focus:border-primary1 sm:text-sm"
               placeholder="Cari admin"
             />
@@ -53,18 +131,14 @@ const HomeDashboardPenggunaAdmin = () => {
         <table className="w-full text-sm text-left rtl:text-right text-neutral3 rounded-lg overflow-hidden">
           <thead className="text-sm text-neutral2 bg-gray-100">
             <tr>
-              <th scope="col" className="p-4">
-                <div className="flex items-center">
-                  <input
-                    id="checkbox-all-search"
-                    type="checkbox"
-                    className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500"
-                  />
-                  <label className="sr-only">checkbox</label>
-                </div>
+              <th scope="col" className="w-4 p-4">
+                <div className="flex items-center">No</div>
               </th>
               <th scope="col" className="px-6 py-3">
                 Nama Admin
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Kode Identitas
               </th>
               <th scope="col" className="px-6 py-3">
                 Email
@@ -76,128 +150,99 @@ const HomeDashboardPenggunaAdmin = () => {
             </tr>
           </thead>
           <tbody>
-            {Array.from({ length: 10 }).map((_, index) => (
-              <tr key={index} className="bg-white border-b hover:bg-gray-50">
-                <td className="w-4 p-4">
-                  <div className="flex items-center">
-                    <input
-                      id="checkbox-table-search-1"
-                      type="checkbox"
-                      className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500"
-                    />
-                    <label className="sr-only">checkbox</label>
-                  </div>
-                </td>
-                <td className="px-6 py-4 font-semibold">Samuel Andrey</td>
-                <td className="px-6 py-4">samuel@gmail.com</td>
-                <td className="px-6 py-4">
-                  <p className="w-max px-4 rounded-sm text-green-600 bg-green-100">Aktif</p>
-                </td>
-                <td className="px-6 py-4">
-                  <div className="flex gap-1">
-                    <Link
-                      href={"/"}
-                      className="block bg-yellow2 p-1 rounded-md fill-white hover:bg-yellow1 transition-all ease-in-out duration-150"
-                    >
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        height="18px"
-                        viewBox="0 0 24 24"
-                        width="18px"
+            {DataAdmin && DataAdmin.data ? (
+              DataAdmin.data.map((admin, index) => (
+                <tr key={index} className="bg-white border-b hover:bg-gray-50">
+                  <td className="w-4 p-4">
+                    <div className="flex items-center">{index + 1}</div>
+                  </td>
+                  <td className="px-6 py-4 font-semibold">{admin.name}</td>
+                  <td className="px-6 py-4">{admin.identity_code}</td>
+                  <td className="px-6 py-4">{admin.email}</td>
+                  <td className="px-6 py-4">
+                    {admin.is_active ? (
+                      <p className="w-max px-4 rounded-sm text-green-600 bg-green-100">
+                        Aktif
+                      </p>
+                    ) : (
+                      <p className="w-max px-4 rounded-sm text-slate-600 bg-slate-100">
+                        Tidak Aktif
+                      </p>
+                    )}
+                  </td>
+                  <td className="px-6 py-4">
+                    <div className="flex gap-1">
+                      <Link
+                        href={{
+                          pathname: `admin/tambah`,
+                          query: {
+                            idAdmin: admin.id,
+                            status: "edit",
+                          },
+                        }}
+                        className="block bg-yellow2 p-1 rounded-md fill-white hover:bg-yellow1 transition-all ease-in-out duration-150"
                       >
-                        <path d="M0 0h24v24H0V0z" fill="none" />
-                        <path d="M3 17.46v3.04c0 .28.22.5.5.5h3.04c.13 0 .26-.05.35-.15L17.81 9.94l-3.75-3.75L3.15 17.1c-.1.1-.15.22-.15.36zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z" />
-                      </svg>
-                    </Link>
-                    <Link
-                      href={"/"}
-                      className="block bg-red2 p-1 rounded-md fill-white hover:bg-red1 transition-all ease-in-out duration-150"
-                    >
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        height="18px"
-                        viewBox="0 0 24 24"
-                        width="18px"
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          height="18px"
+                          viewBox="0 0 24 24"
+                          width="18px"
+                        >
+                          <path d="M0 0h24v24H0V0z" fill="none" />
+                          <path d="M3 17.46v3.04c0 .28.22.5.5.5h3.04c.13 0 .26-.05.35-.15L17.81 9.94l-3.75-3.75L3.15 17.1c-.1.1-.15.22-.15.36zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z" />
+                        </svg>
+                      </Link>
+                      <button
+                        onClick={() => handleOpenModal(admin.id, admin.name)}
+                        className="block bg-red2 p-1 rounded-md fill-white hover:bg-red1 transition-all ease-in-out duration-150"
                       >
-                        <path d="M0 0h24v24H0V0z" fill="none" />
-                        <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V9c0-1.1-.9-2-2-2H8c-1.1 0-2 .9-2 2v10zM18 4h-2.5l-.71-.71c-.18-.18-.44-.29-.7-.29H9.91c-.26 0-.52.11-.7.29L8.5 4H6c-.55 0-1 .45-1 1s.45 1 1 1h12c.55 0 1-.45 1-1s-.45-1-1-1z" />
-                      </svg>
-                    </Link>
-                  </div>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          height="18px"
+                          viewBox="0 0 24 24"
+                          width="18px"
+                        >
+                          <path d="M0 0h24v24H0V0z" fill="none" />
+                          <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V9c0-1.1-.9-2-2-2H8c-1.1 0-2 .9-2 2v10zM18 4h-2.5l-.71-.71c-.18-.18-.44-.29-.7-.29H9.91c-.26 0-.52.11-.7.29L8.5 4H6c-.55 0-1 .45-1 1s.45 1 1 1h12c.55 0 1-.45 1-1s-.45-1-1-1z" />
+                        </svg>
+                      </button>
+                    </div>
+                  </td>
+                </tr>
+              ))
+            ) : (
+              <tr>
+                <td colSpan={6} className="text-center p-4">
+                  Loading data...
                 </td>
               </tr>
-            ))}
+            )}
           </tbody>
         </table>
-        <nav
-          className="flex items-center flex-column flex-wrap md:flex-row justify-between pt-4"
-          aria-label="Table navigation"
-        >
-          <span className="text-sm font-normal text-neutral3 mb-4 md:mb-0 block w-full md:inline md:w-auto">
-            Showing <span className="font-semibold text-gray-900">1-10</span> of{" "}
-            <span className="font-semibold text-gray-900">1000</span>
-          </span>
-          <ul className="inline-flex -space-x-px rtl:space-x-reverse text-sm h-8">
-            <li>
-              <a
-                href="#"
-                className="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-neutral3 bg-white border border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-neutral2"
-              >
-                Sebelumnya
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="flex items-center justify-center px-3 h-8 leading-tight text-neutral3 bg-white border border-gray-300 hover:bg-gray-100 hover:text-neutral2"
-              >
-                1
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="flex items-center justify-center px-3 h-8 leading-tight text-neutral3 bg-white border border-gray-300 hover:bg-gray-100 hover:text-neutral2"
-              >
-                2
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                aria-current="page"
-                className="flex items-center justify-center px-3 h-8 text-blue-600 border border-gray-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700"
-              >
-                3
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="flex items-center justify-center px-3 h-8 leading-tight text-neutral3 bg-white border border-gray-300 hover:bg-gray-100 hover:text-neutral2"
-              >
-                4
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="flex items-center justify-center px-3 h-8 leading-tight text-neutral3 bg-white border border-gray-300 hover:bg-gray-100 hover:text-neutral2"
-              >
-                5
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="flex items-center justify-center px-3 h-8 leading-tight text-neutral3 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-neutral2"
-              >
-                Selanjutnya
-              </a>
-            </li>
-          </ul>
-        </nav>
+        {/* pagination */}
+        <Pagination
+          totalPages={totalPages}
+          currentPage={currentPage}
+          setCurrentPage={setCurrentPage}
+        />
       </div>
+      <Modal
+        title="Hapus Admin"
+        isOpen={isModalOpen}
+        onClose={handleCloseModal}
+      >
+        <div className="mt-4">
+          <p className="mb-4 text-sm">
+            Anda yakin ingin menghapus admin <b>{adminName}</b>?
+          </p>
+          <Button
+            text="Hapus Admin"
+            className="w-full"
+            onClick={handleDelete}
+          />
+        </div>
+      </Modal>
+      <ToastContainer />
     </>
   );
 };
--- app/dashboard/[user]/pengguna/admin/tambah/page.tsx
+++ app/dashboard/[user]/pengguna/admin/tambah/page.tsx
@@ -1,19 +1,224 @@
+"use client";
+
+import {
+  getDetailUserAdmin,
+  postUserAdmin,
+  putUserAdmin,
+} from "@/app/api/superadmin/userAdmin";
 import Button from "@/app/component/general/Button";
 import Input from "@/app/component/general/Input";
-import React from "react";
+import InputBasic from "@/app/component/general/InputBasic";
+import { UpAdminData } from "@/app/interface/UserManagement";
+import { useSearchParams } from "next/navigation";
+import React, { useEffect, useState } from "react";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-const DashboardTambahPenggunaAdminPage = () => {
+const DashboardEditPenggunaAdminPage = () => {
+  const searchParams = useSearchParams();
+  const idAdmin = searchParams.get("idAdmin");
+  const StatusForm = searchParams.get("status");
+
+  const [passwordMatch, setPasswordMatch] = useState(true);
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [isPasswordValid, setIsPasswordValid] = useState(true);
+
+  const [formData, setFormData] = useState<UpAdminData>({
+    id: undefined,
+    identity_code: "",
+    name: "",
+    email: "",
+    password: "",
+    is_active: undefined,
+  });
+
+  const [formDataEdit, setFormDataEdit] = useState<UpAdminData>();
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+
+    if (StatusForm) {
+      setFormDataEdit((prevData) => {
+        const updatedData = {
+          ...prevData,
+          [name]: value,
+        };
+        return updatedData;
+      });
+
+      if (name === "password" || name === "confirmPassword") {
+        setPasswordMatch(
+          name === "confirmPassword"
+            ? value === formDataEdit.password
+            : confirmPassword === value
+        );
+      }
+    } else {
+      // Mode Tambah Baru
+      setFormData((prevData) => ({
+        ...prevData,
+        [name]: value,
+      }));
+
+      if (name === "password") {
+        setIsPasswordValid(value.length >= 8); // Cek panjang password
+      }
+
+      if (name === "password" || name === "confirmPassword") {
+        setPasswordMatch(
+          name === "confirmPassword"
+            ? value === formData.password
+            : confirmPassword === value
+        );
+      }
+    }
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    // Cek apakah password valid
+    if (!isPasswordValid) {
+      toast.error("Password harus minimal 8 karakter");
+      return;
+    }
+
+    // Cek apakah password dan konfirmasi password cocok
+    if (!passwordMatch) {
+      toast.error("Konfirmasi password tidak sesuai");
+      return;
+    }
+    let response;
+
+    if (StatusForm) {
+      // Logika untuk mode edit
+      const { name, identity_code, email, password, is_active } = formDataEdit;
+      if (name && identity_code && email) {
+        try {
+          // Panggil API untuk menambahkan admin baru
+          await putUserAdmin(
+            idAdmin,
+            identity_code,
+            name,
+            email,
+            password,
+            is_active
+          );
+          toast.success("Berhasil mengedit admin!");
+          window.location.href = "./";
+        } catch (error) {
+          toast.error(`Gagal mengedit admin: ${error.message}`);
+        }
+      } else {
+        toast.error("Form belum lengkap");
+      }
+    } else {
+      // Logika untuk mode tambah baru
+      const { name, identity_code, email, password } = formData;
+      if (name && identity_code && email && password) {
+        try {
+          // Panggil API untuk menambahkan admin baru
+          await postUserAdmin(identity_code, name, email, password);
+          toast.success("Berhasil menambahkan admin!");
+          window.location.href = "./";
+        } catch (error) {
+          toast.error(`Gagal menambahkan admin: ${error.message}`);
+        }
+      } else {
+        toast.error("Form belum lengkap");
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (StatusForm && idAdmin) {
+      getDetailUserAdmin(idAdmin)
+        .then((response) => {
+          // Pastikan ini sesuai dengan perubahan tipe response
+          if (response) {
+            const adminData = response.data; // ambil dari array
+
+            setFormDataEdit({
+              id: adminData.id,
+              name: adminData.name,
+              identity_code: adminData.identity_code,
+              email: adminData.email,
+              password: adminData.password,
+              is_active: adminData.is_active,
+            });
+          } else {
+            toast.error("Data admin tidak ditemukan");
+          }
+        })
+        .catch((error) => {
+          console.error("Error fetching admin details:", error);
+
+          // Jika error berasal dari server, tampilkan pesan error dari response
+          if (error.response) {
+            console.error("Error response data:", error.response.data);
+            console.error("Error status:", error.response.status);
+            toast.error(
+              `Gagal memuat detail admin: ${
+                error.response.data.message || "Unknown error"
+              }`
+            );
+          } else if (error.request) {
+            // Jika request dibuat tetapi tidak ada respons
+            console.error("Error request:", error.request);
+            toast.error(
+              "Gagal memuat detail admin: Tidak ada respons dari server."
+            );
+          } else {
+            // Kesalahan lain yang mungkin terjadi
+            console.error("Error message:", error.message);
+            toast.error(`Gagal memuat detail admin: ${error.message}`);
+          }
+        });
+    }
+  }, [StatusForm, idAdmin]);
+
   return (
     <>
-      <form className="max-w-5xl flex flex-col gap-4">
+      <form className="max-w-5xl flex flex-col gap-4" onSubmit={handleSubmit}>
         <div>
-          <p className="font-semibold text-neutral2 text-base">Informasi</p>
+          {StatusForm == "edit" ? (
+            <p className="font-semibold text-neutral2 text-base">Ubah Data</p>
+          ) : (
+            <p className="font-semibold text-neutral2 text-base">Tambah Data</p>
+          )}
+
           <p className="text-neutral3 text-sm">
             Indentitas pengguna role admin
           </p>
           <div className="grid grid-cols-2 gap-x-4 mt-2">
-            <Input type="text" label="Nama" name="nama" required />
-            <Input type="text" label="Email" name="email" required />
+            <Input
+              type="text"
+              label="Nama"
+              name="name"
+              value={StatusForm ? formDataEdit?.name || "" : formData.name}
+              onChange={handleChange}
+              required
+            />
+            <Input
+              type="text"
+              label="Kode Identitas"
+              name="identity_code"
+              value={
+                StatusForm
+                  ? formDataEdit?.identity_code || ""
+                  : formData.identity_code
+              }
+              onChange={handleChange}
+              required
+            />
+            <Input
+              type="text"
+              label="Email"
+              name="email"
+              value={StatusForm ? formDataEdit?.email || "" : formData.email}
+              onChange={handleChange}
+              required
+            />
           </div>
         </div>
 
@@ -21,13 +226,47 @@ const DashboardTambahPenggunaAdminPage = () => {
           <p className="font-semibold text-neutral2 text-base">Autentikasi</p>
           <p className="text-neutral3 text-sm">Password untuk pengguna</p>
           <div className="grid grid-cols-2 gap-x-4 mt-2">
-            <Input type="password" label="Password" name="password" required />
+            <Input
+              type="password"
+              label="Password"
+              name="password"
+              value={StatusForm ? formDataEdit?.password : formData.password}
+              onChange={handleChange}
+              required
+            />
+
             <Input
               type="password"
               label="Konfirmasi Password"
-              name="konfirmasi-password"
+              name="confirmPassword"
+              value={confirmPassword}
+              onChange={(e) => {
+                setConfirmPassword(e.target.value);
+                setPasswordMatch(
+                  StatusForm
+                    ? e.target.value === formDataEdit?.password
+                    : e.target.value === formData.password
+                );
+              }}
               required
             />
+            {!isPasswordValid && (
+              <p className="text-red-600 text-sm">
+                Minimal password 8 karakter
+              </p>
+            )}
+            {/* Pesan validasi di bawah konfirmasi password */}
+            {confirmPassword && (
+              <p
+                className={`text-sm mt-1 ${
+                  passwordMatch ? "text-green-600" : "text-red-600"
+                }`}
+              >
+                {passwordMatch
+                  ? "Password sesuai"
+                  : "Konfirmasi password tidak sesuai"}
+              </p>
+            )}
           </div>
         </div>
 
@@ -36,22 +275,72 @@ const DashboardTambahPenggunaAdminPage = () => {
           <p className="text-neutral3 text-sm">Setup akun aktif atau tidak</p>
           <div className="mt-2 flex gap-4">
             <div className="flex gap-2">
-              <input type="radio" />
+              <input
+                type="radio"
+                id="aktif"
+                name="is_active"
+                value="true"
+                checked={
+                  StatusForm
+                    ? formDataEdit?.is_active === true
+                    : formData.is_active === true
+                }
+                onChange={() => {
+                  if (StatusForm) {
+                    setFormDataEdit((prevData) => ({
+                      ...prevData,
+                      is_active: true,
+                    }));
+                  } else {
+                    setFormData((prevData) => ({
+                      ...prevData,
+                      is_active: true,
+                    }));
+                  }
+                }}
+              />
               <label htmlFor="aktif">Aktif</label>
             </div>
             <div className="flex gap-2">
-              <input type="radio" />
+              <input
+                type="radio"
+                id="tidak-aktif"
+                name="is_active"
+                value="false"
+                checked={
+                  StatusForm
+                    ? formDataEdit?.is_active === false
+                    : formData.is_active === false
+                }
+                onChange={() => {
+                  if (StatusForm) {
+                    setFormDataEdit((prevData) => ({
+                      ...prevData,
+                      is_active: false,
+                    }));
+                  } else {
+                    setFormData((prevData) => ({
+                      ...prevData,
+                      is_active: false,
+                    }));
+                  }
+                }}
+              />
               <label htmlFor="tidak-aktif">Tidak Aktif</label>
             </div>
           </div>
         </div>
 
         <div className="text-end">
-          <Button text="Tambah Admin" />
+          <Button
+            type="submit"
+            text={StatusForm ? "Edit Admin" : "Tambah Admin"}
+          />
         </div>
       </form>
+      <ToastContainer />
     </>
   );
 };
 
-export default DashboardTambahPenggunaAdminPage;
+export default DashboardEditPenggunaAdminPage;
